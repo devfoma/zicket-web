7b5ffbaaeb8bf7c7d948ee43ae04cc97e29526f7
simplicityf <omobolanlehazeezat@gmail.com>
2026-02-24 03:43:18 +0100

    feat(dashboard): add RevenueTrends chart component with hover interactions

--- app/components/organizer/ActivitiesPanel.tsx
+++ app/components/organizer/ActivitiesPanel.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import ZkEmailCenter from './ZkEmailCenter';
 import RecentActivities from './RecentActivities';
+import RevenueTrends from './RevenueTrendsChart';
 
 const ActivitiesPanel = () => {
   return (
-    <section className='grid px-10 py-10 grid-cols-1 md:grid-cols-3 gap-4 bg-[#F8F8F8]'>
-      <ZkEmailCenter />
-      <RecentActivities />
+    <section className='flex flex-col px-10 py-10 gap-4 bg-[#F8F8F8]'>
+
+      {/* Row 1: Revenue Trends + Privacy Split */}
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        <div className='md:col-span-2'>
+          <RevenueTrends />
+        </div>
+        <div className='md:col-span-1'>
+          {/* <PrivacySplit /> */}
+        </div>
+      </div>
+
+      {/* Row 2: ZkEmail Center + Recent Activities */}
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        <div className='md:col-span-1'>
+          <ZkEmailCenter />
+        </div>
+        <div className='md:col-span-2'>
+          <RecentActivities />
+        </div>
+      </div>
+
     </section>
   );
 };
